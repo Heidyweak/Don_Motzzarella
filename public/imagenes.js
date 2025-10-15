96c37e9986a4856e7ffbdb557ef5378a70385b28
Heidyweake <heidy.arian29@gmail.com>
2025-10-15 10:08:35 -0500

    Arreglo

---
+++ public/imagenes.js
@@ -0,0 +1,44 @@
+const pizzas = [
+  {
+    id: 'margarita',
+    name: 'Margarita',
+    desc: 'Salsa de tomate, mozzarella y albahaca fresca.',
+    price: 8.5,
+    img: 'img-margarita.jpg'
+  },
+  {
+    id: 'pepperoni',
+    name: 'Pepperoni',
+    desc: 'Salsa de tomate, mozzarella y pepperoni.',
+    price: 9.0,
+    img: 'img-peperoni.jpg'
+  },
+  {
+    id: 'vegetariana',
+    name: 'Vegetariana',
+    desc: 'Verduras frescas, champiñones, pimientos y aceitunas.',
+    price: 8.0,
+    img: 'img-vegetariana.jpg'
+  },
+  {
+    id: 'cuatroquesos',
+    name: '4 Quesos',
+    desc: 'Mozzarella, gorgonzola, parmesano y fontina.',
+    price: 9.5,
+    img: 'img-4quesos.jpg'
+  },
+  {
+    id: 'napolitana',
+    name: 'Napolitana',
+    desc: 'Tomate, mozzarella, anchoas y orégano.',
+    price: 8.8,
+    img: 'img-napolitana.jpg'
+  },
+  {
+    id: 'hawaiana',
+    name: 'Hawaiana',
+    desc: 'Jamón, piña y mozzarella.',
+    price: 9.2,
+    img: 'img-hawaiana.png'
+  }
+];
